7f4833e59711a74c8a6821cf8324fe67954d58ea
VISHWANATH PRATAP SINGH <49968961+vishnupsingh523@users.noreply.github.com>
2020-05-31 12:41:15 +0530

    All the files are uploaded successfully

---
+++ moveNow.js
@@ -0,0 +1,49 @@
+// This is the first sound of the game start.
+playSound("audio/Super.mp3");
+
+var len = 0;
+var count = document.querySelectorAll("img");
+$("img").click(function(){
+    len++;
+    if(len == count.length)
+    {
+        var ele = document.createElement("h1");
+        ele.innerHTML = "... GAME OVER!!";
+        ele.style.fontSize = "100px";
+        ele.style.fontFamily = "cursive";
+        ele.style.alignSelf = "center";
+        ele.style.textShadow = "5px 5px 10px red";
+        var container = document.querySelector("#images");
+        container.appendChild(ele);
+        $(ele).hide();
+        $(ele).fadeIn(5000);
+        playSound("audio/Fantasy7.mp3");
+    }
+    else{
+        playSound("audio/delicious.mp3")
+    }
+    $(this).hide(500);
+});
+
+
+var mysound;
+function playSound(src){
+    mysound = new sound(src);
+    mysound.play();
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
+}
+
